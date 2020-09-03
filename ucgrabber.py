52bfa3c8b5bcc463b808f79762bc5125d7e45e07
Preston Smith <ptssmith2@gmail.com>
2020-09-02 22:55:42 -0400

    Updated code to match updated website

--- ucgrabber.py
+++ ucgrabber.py
@@ -1,35 +1,40 @@
 # Import libraries
 import time
-import logging
-import smtplib
 from datetime import datetime
-import pandas as pd
-import requests
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
 
-
-url = 'https://www.uc.edu/publichealth/covid-19-dashboard.html'
 
 while True:
 
-    r = requests.get(url)
-    df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
-    df = df_list[0]
-    df2 = df_list[1]
-    data = df.values
-    housingdata = df2.values
+    url = 'https://www.uc.edu/publichealth/covid-19-dashboard.html'
 
-    students = data[0,1]
-    employees = data[1,1]
-    visitors = data[2,1]
-    total_cases = data[3,1]
-    housingcases = housingdata[0,1]
-    offcampuscases = housingdata[1,1]
+    uClient = uReq(url)
+    page_html = uClient.read()
+    uClient.close()
+
+    #finds all p html elements
+    page_soup = soup(page_html,'lxml')
+    tags = page_soup.find_all('p')
+    type(tags)
+
+    #Seperates numbers from text
+    housingdata = tags[6].text
+    splithousingdata = housingdata.split()
+
+    #sorts p html elements
+    students = tags[5].text
+    employees = tags[7].text
+    visitors = tags[8].text
+    total_cases = tags[4].text
+    housingcases = splithousingdata[0]
+    offcampuscases = splithousingdata[5]
 
     file = open('ucdata.txt','w')
     #clears contents of the file
     file.truncate()
     #writes data to .txt file
-    file.write(str(students)+'\n'+str(employees)+'\n'+str(visitors)+'\n'+str(total_cases)+'\n'+str(housingcases)+'\n'+str(offcampuscases)+'\n')
+    file.write(str(students)+str(employees)+str(visitors)+str(total_cases)+str(housingcases)+'\n'+str(offcampuscases)+'\n')
     #closes file
     file.close()
 
@@ -37,8 +42,8 @@ while True:
     now = datetime.now()
     print('Data read at: ',now)
 
-    #sleep for a day
-    time.sleep(86400)
+    #sleep for 2 hours
+    time.sleep(7200)
 
 
 
