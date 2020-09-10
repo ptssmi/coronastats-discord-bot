d8f0abe32d585abe5b5f23fe03170cbc52f6a11f
Preston Smith <ptssmith2@gmail.com>
2020-09-09 22:28:53 -0400

    seeing if it captures the .jpg

---
+++ picturetaker.py
@@ -0,0 +1,3 @@
+import imgkit 
+
+imgkit.from_url('https://dataanalytics.uc.edu/t/Provost/views/UCCOVIDCareful-External/CaseSummaryExternal?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fdataanalytics.uc.edu%2F&%3Aembed_code_version=3&%3Atabs=no&%3Atoolbar=yes&%3Aiid=5&%3AisGuestRedirectFromVizportal=y&%3Adisplay_spinner=no&%3AloadOrderID=0','ucdata.jpg')
